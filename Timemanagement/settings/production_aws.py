e54fd97c4b0a3821a3257f923320229d8212d5aa
Emmastro <emmamurairi@gmail.com>
2020-05-02 18:55:39 +0200

    Hoting settings for AWS

--- Timemanagement/settings/production_aws.py
+++ Timemanagement/settings/production_aws.py
@@ -1,3 +1,16 @@
 from .base import *
+import os
+import dj_database_url
+from .base import*
 
 ALLOWED_HOSTS = ['192.0.2.143']
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
+}
