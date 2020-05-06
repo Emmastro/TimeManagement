3f1180fb4b2799a9dda0e2bb51b633002a2a1343
Emmastro <emmamurairi@gmail.com>
2020-05-06 21:33:39 +0200

    Config postgresql

--- Timemanagement/settings/production_aws.py
+++ Timemanagement/settings/production_aws.py
@@ -9,22 +9,12 @@ ALLOWED_HOSTS = ['3.223.180.215']
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
-}
-
-
-"""
-DATABASES = {
-'default': {
+    'default': {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
- 'NAME': 'djangostack',
-'HOST': '/opt/bitnami/postgresql',
-'PORT': '5432',
- 'USER': 'bitnami',
- 'PASSWORD': '2425de1e84'
+    'NAME': 'timemanagement',
+    'HOST': 'ls-da52e14c356faa9756bf02bb29eaa12c26d02480.cc2syslbeiea.us-east-1.rds.amazonaws.com',
+    'PORT': '5432',
+    'USER': 'dbmasteruser',
+    'PASSWORD': '4lB<B%9e!V&0U~Hl*EP]z];Q2HBEY!E7'
+    }
 }
-}
-"""
