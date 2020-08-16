1c3cf2461112736285347dc3cf768758d9f820b9
Emmastro <emurairi18@alastudents.org>
2020-08-16 22:07:52 +0200

    Design new html base templates

--- export dashboard.sh
+++
@@ -1,8 +0,0 @@
-cd $1
-sed -i 's/assets/\/static\/BootstrapAdmin/g' index.html
-sed -i 's/assets/\/static\/BootstrapAdmin/g' period.html
-sed -i 's/assets/\/static\/BootstrapAdmin/g' 404.html
-mv index.html templates/admin_.html
-mv period.html templates/period_.html
-mv 404.html templates/404_.html
-mv assets static/BootstrapAdmin
--- export.sh
+++ export.sh
@@ -1,4 +1,10 @@
 cd $1
-sed -i 's/assets/static/g' index.html
-mv index.html templates/home.html
-mv assets static
+sed -i 's/assets/\/static\/BootstrapAdmin/g' newTemplate.html
+sed -i 's/assets/\/static\/BootstrapAdmin/g' setupCalendar.html
+sed -i 's/assets/\/static\/BootstrapAdmin/g' templates.html
+sed -i 's/assets/\/static\/BootstrapAdmin/g' 404.html
+mv newTemplate.html templates/base/newTemplate.html
+mv setupCalendar.html templates/base/setupCalendar.html
+mv templates.html templates/base/templates.html
+mv 404.html templates/base/404.html
+mv assets static/BootstrapAdmin
